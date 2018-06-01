dbd0248c2b8d1198d6b0bacc3679fe7d33342925
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-06-01 11:26:02 +0200

    spring-cloud-contract. First boot project from start.spring.io: car-rental

---
+++ car-rental/src/test/java/com/malsolo/springframework/cloud/contract/carrental/CarRentalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.malsolo.springframework.cloud.contract.carrental;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarRentalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
